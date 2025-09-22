b18261ab72618bd3e0588d8e55006a6094ec7540
Exor-hue <judesmithudoh@gmail.com>
2025-09-22 08:34:30 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Supreme',
   author: '‎supreme',
   botName: "𝐉ᴜɴᴇ 𝐌ᴅ",
-  botOwner: 'supreme', // Your name
-  ownerNumber: '25479201944', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'Ꮋ͛ᵁ̼̽ᴺ̼̽᚜ᚸ⃝⃘⃟⃠̰̃᚛ᵀ̼̽ᴱ̼̽Ꮢ', // Your name
+  ownerNumber: '2349056476729', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
